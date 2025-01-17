d2b067ef2e150822885fa8ad1dfb47c9b634efd4
Tyler Campbell <tylercampbell@users.noreply.github.com>
2025-01-17 18:05:51 -0500

    initial blog posts

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -1,6 +1,13 @@
+import slugify from "slugify";
+
 export default {
   tags: [
     "posts"
   ],
   "layout": "post.njk",
+  eleventyComputed: {
+    permalink: data => {
+      return `/blog/${slugify(data.page.fileSlug, { lower: true })}/`;
+    }
+  }
 };
